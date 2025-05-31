535b9ff00b1cc76c527299e97642155b35ad879c
João Paulo <155689082+OLIVEIRA028@users.noreply.github.com>
2025-05-31 15:21:32 +0000

    Projeto

--- src/main/java/bibliotecaFacil/servlets/UsuarioServlet.java
+++ src/main/java/bibliotecaFacil/servlets/UsuarioServlet.java
@@ -1,21 +1,42 @@
-protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+package bibliotecaFacil.servlets;
 
-    String nome = request.getParameter("nome");
-    int matricula = Integer.parseInt(request.getParameter("matricula"));
-    String cpf = request.getParameter("cpf");
-    String email = request.getParameter("email");
-    String tipo = request.getParameter("tipo");
+import bibliotecaFacil.dao.UsuarioDAO;
+import bibliotecaFacil.modelo.Aluno;
+import bibliotecaFacil.modelo.Professor;
+import bibliotecaFacil.modelo.Usuario;
 
-    Usuario usuario;
-    if ("Aluno".equalsIgnoreCase(tipo)) {
-        usuario = new Aluno(matricula, nome, cpf, email);
-    } else {
-        usuario = new Professor(matricula, nome, cpf, email);
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UsuarioServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String nome = request.getParameter("nome");
+        int matricula = Integer.parseInt(request.getParameter("matricula"));
+        String cpf = request.getParameter("cpf");
+        String email = request.getParameter("email");
+        String tipo = request.getParameter("tipo");
+
+        Usuario usuario;
+        if ("Aluno".equalsIgnoreCase(tipo)) {
+            usuario = new Aluno(matricula, nome, cpf, email);
+        } else {
+            usuario = new Professor(matricula, nome, cpf, email);
+        }
+
+        UsuarioDAO dao = new UsuarioDAO();
+        dao.inserir(usuario);
+
+        response.setContentType("text/html");
+        response.getWriter().println("<html><body>");
+        response.getWriter().println("<h2>Usuário cadastrado com sucesso!</h2>");
+        response.getWriter().println("<a href=\"index.html\">Voltar</a>");
+        response.getWriter().println("</body></html>");
     }
-
-    UsuarioDAO dao = new UsuarioDAO();
-    dao.inserir(usuario);
-
-    response.sendRedirect("sucesso.html");
 }
