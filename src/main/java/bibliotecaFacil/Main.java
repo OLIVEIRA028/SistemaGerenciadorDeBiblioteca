ae4815ea6965c37d8150c944b0447b0375918352
João Paulo <155689082+OLIVEIRA028@users.noreply.github.com>
2025-05-31 14:11:56 +0000

    Projeto

--- src/main/java/bibliotecaFacil/Main.java
+++ src/main/java/bibliotecaFacil/Main.java
@@ -2,10 +2,39 @@ package bibliotecaFacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/cadastroLivro")
+    public String cadastroLivro() {
+        return "cadastroLivro";
+    }
+
+    @GetMapping("/cadastroUsuario")
+    public String cadastroUsuario() {
+        return "cadastroUsuario";
+    }
+
+    @GetMapping("/listarLivros")
+    public String listarLivros() {
+        return "listarLivros";
+    }
+
+    @GetMapping("/registrarEmprestimoDevolucao")
+    public String registrarEmprestimoDevolucao() {
+        return "registrarEmprestimoDevolucao";
     }
 }
